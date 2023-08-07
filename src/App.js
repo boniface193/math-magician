6771d93ca437e8f943fa26e7e3a7bf33b0bb714a
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-07 15:35:01 +0100

    calculator added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { BrowserRouter as CreateElement } from 'react-router-dom';
+import Calculator from './components/Calculator';
+import './style.css';
+
 function App() {
   return (
-    <>home</>
+    <CreateElement>
+      <Calculator />
+    </CreateElement>
   );
 }
 
