5d1d1d6776b141bef1cfa773451d7f963c5aae4b
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-07 16:22:55 +0100

    calculator completed with big js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter as CreateElement, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
-import Quotes from './components/Quote';
 import './style.css';
 
 function App() {
   return (
     <CreateElement>
-      <Layout />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quotes />} />
       </Routes>
     </CreateElement>
   );
